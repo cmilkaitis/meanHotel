6278d58b886da81abace9a9fa65ba774169f4b5f
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-29 11:16:40 -0400

    Lesson 8 - Setting up routing

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express');
 let app = express();
+let path = require('path');
 
 app.set('port', 3000);
+
+app.get('/', function(req, res){
+    console.log("GET the homepage");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res){
+    console.log("GET the json");
+    res
+        .status(200)
+        .json({"Expressing myself": true});
+});
+
+app.get('/file', function(req, res){
+    console.log("GET the file");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
 
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
