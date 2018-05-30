4f2a7462bf1862e4dd4c1e3fbd39a60a14fbdeac
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-30 12:12:16 -0400

    Lesson 11 - Using the Express router

--- app.js
+++ app.js
@@ -14,20 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
 
-app.get('/json', function(req, res){
-    console.log("GET the json");
-    res
-        .status(200)
-        .json({"JSONdata": true});
-});
-
-app.get('/file', function(req, res){
-    console.log("GET the file");
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'app.js'));
-});
-
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
     console.log('working on ' + port);
