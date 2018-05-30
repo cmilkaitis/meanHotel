72baaeba630770cbbe773e256a9f1b6bd1763393
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-30 12:59:51 -0400

    Lesson 13 - Defining dynamic routes with URL params

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -6,3 +6,12 @@ module.exports.hotelsGetAll = function(req, res) {
         .status(200)
         .json( hotelData );
 };
+
+module.exports.hotelsGetOne = function(req, res) {
+    let hotelId = req.params.hotelID;
+    var thisHotel = hotelData[hotelId];
+    console.log("GET the hotel ID: ", hotelId);
+    res
+        .status(200)
+        .json( thisHotel );
+};
--- api/routes/index.js
+++ api/routes/index.js
@@ -5,5 +5,9 @@ let ctrlHotels =require('../controllers/hotels.controllers.js');
 router
     .route('/hotels')
     .get(ctrlHotels.hotelsGetAll);
+
+router
+    .route('/hotels/:hotelID')
+    .get(ctrlHotels.hotelsGetOne);
    
 module.exports = router;
